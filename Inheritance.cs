17a73650ff8431840f4b45349d37b5de4443fcf1
SethAkoto <sethakotodid@gmail.com>
2025-08-08 16:01:54 +0000

    Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+    
+        genericAnimal.MakeSound(); 
+        dog.MakeSound();           
+        cat.MakeSound();           
+    }
+}
